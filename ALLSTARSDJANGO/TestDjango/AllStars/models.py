f0e55a291f79a0184360ebb03e27214b2cbe843f
ignaciooyarzo <ignaciooyarzo826@gmail.com>
2021-06-21 22:11:43 -0400

    Se crearon los archivos forms y url

--- ALLSTARSDJANGO/TestDjango/AllStars/models.py
+++ ALLSTARSDJANGO/TestDjango/AllStars/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+#Create your models here.
 
 class Categoria(models.Model):
     idCategoria = models.IntegerField(primary_key=True, verbose_name='Id de categoria')
